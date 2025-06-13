541f81492d0cb4996a0799ab833fe80a2ebc6c03
Aayush Prasai <gayatriprasai@gmail.com>
2025-06-13 13:52:04 +0545

    Changes in notes

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import Http404
 from . models import Notes
+from django.views.generic import ListView
+
+class NotesListView(ListView):
+    model=Notes
+    context_context_name='notes'
+
 # Create your views here.
 
-def list(request):
-    all_notes= Notes.objects.all()
-    return render(request,'notes/notes_list.html',{'notes':all_notes})
 
 def detail(request, pk):
     try:
