7d248d151702291ffed08c291da26960bd2fa304
Aayush Prasai <gayatriprasai@gmail.com>
2025-06-13 18:01:10 +0545

    Added Auth

--- notes/views.py
+++ notes/views.py
@@ -5,7 +5,7 @@ from . models import Notes
 from .forms import NotesForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 def add_like_view(request,pk):
    if request.method == 'POST':
@@ -15,42 +15,58 @@ def add_like_view(request,pk):
     return HttpResponseRedirect(reverse('notes.detail', args=(pk,)))
    raise Http404
 
-class NotesDeleteView(DeleteView):
+
+class NotesDeleteView(LoginRequiredMixin,DeleteView):
     model=Notes
     success_url='/smart/notes'
     template_name= 'notes/notes_delete.html'
+    login_url="/admin"
 
 
-class NotesUpdateView(UpdateView):
+class NotesUpdateView(LoginRequiredMixin,UpdateView):
     model=Notes
     success_url='/smart/notes'
     form_class = NotesForm
+    login_url="/admin"
 
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin,CreateView):
     model=Notes
     success_url='/smart/notes'
     form_class = NotesForm
+    login_url="/admin"
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user= self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin,ListView):
     model= Notes
     context_object_name="notes"
     template_name = "notes/notes_list.html"
+    login_url="/admin"
 
-class PopularNotesListView(ListView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class PopularNotesListView(LoginRequiredMixin,ListView):
     model= Notes
     context_object_name="notes"
     template_name = "notes/notes_list.html"
     queryset = Notes.objects.filter(pk__gte=1)
+    login_url="/admin"
 
 # def list(request):
 #     all_notes= Notes.objects.all()
 #     return render(request,'notes/notes_list.html',{'notes':all_notes})
 
 
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin,DetailView):
     model = Notes
     context_object_name= 'note'
+    login_url="/admin"
 # def detail(request, pk):
 #     try:
 #         note = Notes.objects.get(pk=pk)
